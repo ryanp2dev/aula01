4a0e0bb35dd41ea405392af1416e2887e315a923
ryanp2dev <65263907+ryanp2dev@users.noreply.github.com>
2025-04-07 01:26:34 +0000

    excluir porra

--- src/main/java/com/fourcatsdev/aula01/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fourcatsdev.aula01.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fourcatsdev.aula01.modelo.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	
-}
